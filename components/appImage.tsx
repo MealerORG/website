57aab80ce7a646f986258846ba8dd7733377e4c7
Nelson Gou <nel4gou@gmail.com>
2023-03-12 13:48:46 -0700

    Add download page and AppImage component

---
+++ components/appImage.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+
+export default function AppImage(props: any) {
+    const frame = {
+        src: "/app/frame.png",
+        width: 1240,
+        height: 2500,
+        alt: "iPhone Frame"
+    };
+
+    const app = {
+        src: `/app/${props.app}.png`,
+        width: 1084,
+        height: 2345,
+        alt: "Mealer App Image"
+    };
+
+    const appInset = {
+        left: "6.4%",
+        right: "6.4%",
+        top: "3.2%",
+        bottom: "3.2%"
+    };
+
+    return <>
+        <div className="position-relative" style={{ aspectRatio: `${frame.width} / ${frame.height}`, ...props.style }}>
+            <Image className="position-absolute" src={frame.src} alt={frame.alt} fill style={{ objectFit: "contain", zIndex: "1" }} />
+            <div className="position-absolute" style={{...appInset , aspectRatio: `${app.width} / ${app.height}` }}>
+                <Image className="position-absolute" src={app.src} alt={app.alt} fill style={{ objectFit: "contain" }} />
+            </div>
+        </div>
+    </>;
+}
--- pages/download.tsx
+++ pages/download.tsx
@@ -1,5 +1,34 @@
+import Image from "next/image";
+import AppImage from "../components/appImage";
+
 export default function Download() {
+    const platforms = [
+        { name: "iOS", href: "/download/ios", image: "/app/download-ios.svg", width: 150, height: 50, alt: "Apple App Store" },
+        { name: "Android", href: "/download/android", image: "/app/download-android.svg", width: 170, height: 50, alt: "Google Play Store" }
+    ];
+
     return <>
-        download!
+        <div className="row mx-0">
+            <div className="col-sm-6 col-md-8 col-xxl-9 mt-3 d-flex flex-column justify-content-center">
+                <div className="text-center">
+                    <h1>Download the App</h1>
+
+                    <p style={{ fontSize: "1.125em" }}>Start meal planning, saving money, and helping the environment today!</p>
+
+                    <div className="d-flex flex-wrap justify-content-center">
+                        {platforms.map((platform) =>
+                            <a href={platform.href} target="_blank" rel="noopener noreferrer" className="m-1">
+                                <Image src={platform.image} width={platform.width} height={platform.height} alt={platform.alt} />
+                            </a>
+                        )}
+                    </div>
+
+                    <div className="mt-3"><em>Desktop and web versions are coming soon.</em></div>
+                </div>
+            </div>
+            <div className="col-sm-6 col-md-4 col-xxl-3 mt-3">
+                <AppImage app="login" />
+            </div>
+        </div>
     </>;
 }
